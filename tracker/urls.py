eaa45bb08de8e40275ef294d512259fe5aef2348
Vanessa <vmarieharmon@gmail.com>
2023-07-25 11:57:28 -0500

    Feature 15 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -24,6 +24,7 @@ def redirect_to_project_list(request):
 
 
 urlpatterns = [
+    path("tasks/", include("tasks.urls")),
     path("accounts/", include("accounts.urls")),
     path("", redirect_to_project_list, name="home"),
     path("projects/", include("projects.urls")),
